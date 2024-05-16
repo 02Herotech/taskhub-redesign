9ce50a89df1179479efe191cdc9da4456b671724
krystein <Nworahchimaobic@gmail.com>
2024-05-16 21:57:01 +0100

    taskhub

--- src/components/auth/LoginForm/index.tsx
+++ src/components/auth/LoginForm/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Button from "@/components/global/Button";
 import Input from "@/components/global/Input";
@@ -11,116 +11,120 @@ import { useSigninMutation } from "@/services/auth";
 import { useState } from "react";
 
 type SignInRequest = {
-    emailAddress: string;
-    password: string;
+  emailAddress: string;
+  password: string;
 };
 
 const LoginForm = () => {
-    const [loading, setLoading] = useState(false);
-    const router = useRouter();
-    const methods = useForm({
-        mode: "onChange",
-        defaultValues: {
-            emailAddress: "",
-            password: "",
-        },
-    });
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const methods = useForm({
+    mode: "onChange",
+    defaultValues: {
+      emailAddress: "",
+      password: "",
+    },
+  });
 
-    const {
-        formState: { errors, isValid },
-    } = methods;
+  const {
+    formState: { errors, isValid },
+  } = methods;
 
-    const searchParams = useSearchParams();
+  const searchParams = useSearchParams();
 
-    const from = searchParams.get("from");
+  const from = searchParams.get("from");
 
-    const session = useSession();
-    console.log(session)
+  const session = useSession();
+  console.log(session);
 
-    const [_signin, { isLoading }] = useSigninMutation();
+  const [_signin, { isLoading }] = useSigninMutation();
 
-    const onSubmit: SubmitHandler<SignInRequest> = async (payload) => {
-        try {
-            const response = await _signin(payload).unwrap();
-            setLoading(true);
+  const onSubmit: SubmitHandler<SignInRequest> = async (payload) => {
+    try {
+      const response = await _signin(payload).unwrap();
+      setLoading(true);
 
-            const result = await signIn('credentials', {
-                redirect: false,
-                accessToken: response.accessToken,
-                refreshToken: response.refreshToken,
-                user: response.user
-            });
+      const result = await signIn("credentials", {
+        redirect: false,
+        accessToken: response.accessToken,
+        refreshToken: response.refreshToken,
+        user: response.user,
+      });
 
-            console.log(result)
+      console.log(result);
+    } catch (err: any) {
+      toast.error(err?.data.message || "Invalid credentials");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-            
-        } catch (err: any) {
-            toast.error(err?.data.message || "Invalid credentials");
-        } finally {
-            setLoading(false);
-        }
-    };
+  return (
+    <section className="w-full xl:w-[554px] mx-auto max-lg:p-10">
+      <div className="space-y-10">
+        <div className="space-y-4 font-clashDisplay">
+          <h1 className="text-2xl lg:text-4xl text-black font-medium">
+            Welcome to{" "}
+            <span className="text-primary">
+              <b>Task</b>hub
+            </span>
+          </h1>
+          <p className="text-xl lg:text-2xl text-tc-gray font-medium">
+            Join us for exclusive access to our services
+          </p>
+        </div>
 
-    return (
-        <section className='w-full xl:w-[554px] mx-auto max-lg:p-10'>
-            <div className='space-y-10'>
-                <div className="space-y-4 font-clashDisplay">
-                    <h1 className='text-2xl lg:text-4xl text-black font-medium'>
-                        Welcome to{" "}
-                        <span className="text-primary"><b>Task</b>hub</span>
-                    </h1>
-                    <p className='text-xl lg:text-2xl text-tc-gray font-medium'>
-                        Join us for exclusive access to our services
-                    </p>
-                </div>
-
-                <FormProvider {...methods}>
-                    <form
-                        onSubmit={methods.handleSubmit(onSubmit)}
-                        className='w-full space-y-5 font-satoshi'>
-                        <Input
-                            focused
-                            name='emailAddress'
-                            label='Email address'
-                            placeholder='example@example.com'
-                            rules={["email", "required"]}
-                            type='email'
-                            className="shadow-sm"
-                        />
-                        <Input
-                            name='password'
-                            label='Password'
-                            placeholder='password'
-                            rules={["required"]}
-                            type='password'
-                            className="shadow-sm"
-                        />
-                        <div className='pt-1 space-y-5'>
-                            <div className="flex items-center justify-end">
-                                <Button
-                                    underline={false}
-                                    tag='a'
-                                    href='/auth/forgot-password'
-                                    className='flex items-center underline font-bold'>
-                                    Forgot password
-                                </Button>
-                            </div>
-                            <Button
-                                type='submit'
-                                loading={loading}
-                                disabled={!isValid}
-                                className='w-full lg:w-[170px] rounded-full font-normal'>
-                                Log in
-                            </Button>
-                            <h3 className="text-xl font-bold">Don’t have an account?
-                                <Link href="/auth/sign-up" className="text-primary"> Sign Up</Link>
-                            </h3>
-                        </div>
-                    </form>
-                </FormProvider>
+        <FormProvider {...methods}>
+          <form
+            onSubmit={methods.handleSubmit(onSubmit)}
+            className="w-full space-y-5 font-satoshi">
+            <Input
+              focused
+              name="emailAddress"
+              label="Email address"
+              placeholder="example@example.com"
+              rules={["email", "required"]}
+              type="email"
+              className="shadow-sm"
+            />
+            <Input
+              name="password"
+              label="Password"
+              placeholder="password"
+              rules={["required"]}
+              type="password"
+              className="shadow-sm"
+            />
+            <div className="pt-1 space-y-5">
+              <div className="flex items-center justify-end">
+                <Button
+                  underline={false}
+                  tag="a"
+                  href="/auth/forgot-password"
+                  className="flex items-center underline font-bold">
+                  Forgot password
+                </Button>
+              </div>
+              <Button
+                type="submit"
+                loading={loading}
+                disabled={!isValid}
+                className="w-full lg:w-[170px] rounded-full font-normal">
+                Log in
+              </Button>
+              <h3 className="text-xl font-bold">
+                Don’t have an account?
+                <Link href="/auth/sign-up" className="text-primary">
+                  {" "}
+                  Sign Up
+                </Link>
+              </h3>
             </div>
-        </section>
-    );
+          </form>
+        </FormProvider>
+      </div>
+    </section>
+  );
 };
 
 export default LoginForm;
--- src/components/layout/Navigation/index.tsx
+++ src/components/layout/Navigation/index.tsx
@@ -12,128 +12,138 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 import { BiChevronDown } from "react-icons/bi";
 import { RiMenu3Fill } from "react-icons/ri";
 import Dropdown from "@/components/global/Dropdown";
-import { signOut } from "next-auth/react"
+import { signOut } from "next-auth/react";
 
 const Navigation = () => {
-    const router = useRouter();
-    const [showMobileNav, setShowMobileNav] = useState(false);
+  const router = useRouter();
+  const [showMobileNav, setShowMobileNav] = useState(false);
 
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    const handleLogout = async () => {
-        await signOut();
-        router.push("/auth/login");
-    } 
+  const handleLogout = async () => {
+    await signOut();
+    router.push("/auth/login");
+  };
 
-    const links = [
-        {
-            label: "Add a task",
-            url: "/customer/add-task",
-        },
-        {
-            label: "Explore Tasks",
-            url: "/explore",
-        },
-        {
-            label: "Marketplace",
-            url: "/marketplace",
-        },
-        {
-            label: "Provider a service",
-            url: "/provider-service",
-        }
-    ];
+  const links = [
+    {
+      label: "Add a task",
+      url: "/customer/add-task",
+    },
+    {
+      label: "Explore Tasks",
+      url: "/explore",
+    },
+    {
+      label: "Marketplace",
+      url: "/marketplace",
+    },
+    {
+      label: "Provider a service",
+      url: "/provider-service",
+    },
+  ];
 
-    const dropdownItems = [
-        {
-            label: "Profile",
-            onClick: () => { },
-        },
-        {
-            label: "Settings",
-            onClick: () => { },
-        },
-        {
-            label: "Logout",
-            onClick: handleLogout,
-        },
-    ];
+  const dropdownItems = [
+    {
+      label: "Profile",
+      onClick: () => {},
+    },
+    {
+      label: "Settings",
+      onClick: () => {},
+    },
+    {
+      label: "Logout",
+      onClick: handleLogout,
+    },
+  ];
 
-    return (
-        <>
-            <nav className='bg-white z-50 fixed top-0 left-0 right-0 w-full drop-shadow-sm'>
-                <div className='container py-4 lg:py-5 px-7 lg:px-14 flex items-center justify-between'>
-                    <Link href='/' className='w-[67px] h-[50px] lg:w-[109px] relative'>
-                        <Image src="/assets/images/logo.png" fill alt="Logo" />
-                    </Link>
-                    <button
-                        onClick={() => setShowMobileNav((state) => !state)}
-                        className='lg:hidden'>
-                        <RiMenu3Fill className="text-primary w-9 h-9" />
-                    </button>
-                    <ul className='hidden lg:flex items-center space-x-8'>
-                        {links.map((link) => {
-                            return (
-                                <li key={link.url} className="relative">
-                                    <Link href={link.url as string} className={cn("text-primary text-xl font-semibold", {
-                                        "text-tc-orange":
-                                            link.url === "/" && pathname === "/"
-                                                ? true
-                                                : link.url !== "/" && pathname.includes(link.url)
-                                                    ? true
-                                                    : false,
-                                    })}>
-                                        {link.label}
-                                    </Link>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                    <div className='hidden lg:flex items-center space-x-5'>
-                        <div className="relative">
-                            <BsChat className="size-[22px] text-black" />
-                            <div className="bg-tc-orange absolute size-5 flex items-center justify-center text-xs -top-1 -right-1 rounded-full text-white">2</div>
-                        </div>
-                        <div className="relative">
-                            <IoMdNotificationsOutline className="size-[24px] text-black" />
-                            <div className="bg-tc-orange absolute size-5 flex items-center justify-center text-xs -top-1 -right-1 rounded-full text-white">2</div>
-                        </div>
-                        <Dropdown
-                            trigger={() => (
-                                <div className="flex items-center space-x-1">
-                                    <div className="relative size-[46px] rounded-full border">
-                                        <Image src="/assets/images/logo.png" fill alt="Logo" className="object-contain" />
-                                    </div>
-                                    <BiChevronDown className="size-6" />
-                                </div>
-                            )}
-                            className='-left-32 top-20'>
-                            <div className='w-[200px] bg-white rounded-md'>
-                                {dropdownItems.map((button, index) => (
-                                    <button
-                                        key={index}
-                                        onClick={button.onClick}
-                                        className='flex w-full dropdown-item hover:bg-status-lightViolet transition-all text-md items-center justify-between p-3'>
-                                        {button.label}
-                                    </button>
-                                ))}
-                            </div>
-                        </Dropdown>
-
-                    </div>
-                </div>
-            </nav>
-            <AnimatePresence initial={false}>
-                {showMobileNav && (
-                    <MobileNavigation
-                        setShowMobileNav={setShowMobileNav}
-                        showMobileNav={showMobileNav}
-                        links={links}
+  return (
+    <>
+      <nav className="bg-white z-50 fixed top-0 left-0 right-0 w-full drop-shadow-sm">
+        <div className="container h-5 py-4 lg:py-5 px-7 lg:px-14 flex items-center justify-between">
+          <Link href="/" className="w-[67px] h-[50px] lg:w-[109px] relative">
+            <Image src="/assets/images/logo.png" fill alt="Logo" />
+          </Link>
+          <button
+            onClick={() => setShowMobileNav((state) => !state)}
+            className="lg:hidden">
+            <RiMenu3Fill className="text-primary w-9 h-9" />
+          </button>
+          <ul className="hidden lg:flex items-center space-x-8">
+            {links.map((link) => {
+              return (
+                <li key={link.url} className="relative">
+                  <Link
+                    href={link.url as string}
+                    className={cn("text-primary text-xl font-semibold", {
+                      "text-tc-orange":
+                        link.url === "/" && pathname === "/"
+                          ? true
+                          : link.url !== "/" && pathname.includes(link.url)
+                          ? true
+                          : false,
+                    })}>
+                    {link.label}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="hidden lg:flex items-center space-x-5">
+            <div className="relative">
+              <BsChat className="size-[22px] text-black" />
+              <div className="bg-tc-orange absolute size-5 flex items-center justify-center text-xs -top-1 -right-1 rounded-full text-white">
+                2
+              </div>
+            </div>
+            <div className="relative">
+              <IoMdNotificationsOutline className="size-[24px] text-black" />
+              <div className="bg-tc-orange absolute size-5 flex items-center justify-center text-xs -top-1 -right-1 rounded-full text-white">
+                2
+              </div>
+            </div>
+            <Dropdown
+              trigger={() => (
+                <div className="flex items-center space-x-1">
+                  <div className="relative size-[46px] rounded-full border">
+                    <Image
+                      src="/assets/images/logo.png"
+                      fill
+                      alt="Logo"
+                      className="object-contain"
                     />
-                )}
-            </AnimatePresence>
-        </>
-    );
+                  </div>
+                  <BiChevronDown className="size-6" />
+                </div>
+              )}
+              className="-left-32 top-20">
+              <div className="w-[200px] bg-white rounded-md">
+                {dropdownItems.map((button, index) => (
+                  <button
+                    key={index}
+                    onClick={button.onClick}
+                    className="flex w-full dropdown-item hover:bg-status-lightViolet transition-all text-md items-center justify-between p-3">
+                    {button.label}
+                  </button>
+                ))}
+              </div>
+            </Dropdown>
+          </div>
+        </div>
+      </nav>
+      <AnimatePresence initial={false}>
+        {showMobileNav && (
+          <MobileNavigation
+            setShowMobileNav={setShowMobileNav}
+            showMobileNav={showMobileNav}
+            links={links}
+          />
+        )}
+      </AnimatePresence>
+    </>
+  );
 };
 
 export default Navigation;
