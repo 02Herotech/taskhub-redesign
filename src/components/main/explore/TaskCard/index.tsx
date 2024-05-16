43e241a52f63aecb41e4db05c11063a96779d2d4
Ntami Egbe <77502425+ntamiegbe@users.noreply.github.com>
2024-05-16 07:07:54 +0100

    fix: task card

--- src/components/main/explore/TaskCard/index.tsx
+++ src/components/main/explore/TaskCard/index.tsx
@@ -1,54 +1,58 @@
-import Image from "next/image";
-import { HiOutlineLocationMarker } from "react-icons/hi";
-import { FiCalendar, FiClock } from "react-icons/fi";
-import { motion } from "framer-motion";
-import { formatAmount } from "@/lib/utils";
+"use client"
+
+import { formatAmount, formatDate } from "@/lib/utils";
 import { Task } from "@/types/services/tasks";
 import { useRouter } from "next/navigation";
+import Image from 'next/image';
+import { FiCalendar, FiClock } from 'react-icons/fi'; 
+import { HiOutlineLocationMarker } from 'react-icons/hi'; 
+import { motion } from 'framer-motion';
 
-const TaskCard = ({taskServiceName, taskImage, taskDates, active, customerBudget, postedAt, id}: Task) => {
-    const location = "Queensland, Australia";
-    const date = new Date(postedAt).toLocaleDateString();
-    const time = new Date(postedAt).toLocaleTimeString();
-    const availabiliy = active ? "Available" : "Unavailable";
+// interface TaskCardProps {
+//     task: Task;
+// }
+
+const TaskCard = ({ task }: any) => {
     const router = useRouter();
+
+    const availability = task.active ? "Available" : "Unavailable";
 
     return (
         <motion.div
             className="bg-[#EBE9F4] p-5 rounded-xl lg:rounded-4xl cursor-pointer"
             whileHover={{ scale: 1.02 }}
-            onClick={() => router.push(`/explore/task-details/${id}`)}
+            onClick={() => router.push(`/explore/task-details/${task.id}`)}
         >
             <div className="flex items-center justify-between w-full">
-                <h2 className="text-2xl lg:text-[32px] text-primary font-bold">{taskServiceName}</h2>
+                <h2 className="text-2xl lg:text-[32px] text-primary font-bold">{task.taskServiceName}</h2>
                 <div className='size-[46px] rounded-full border relative'>
-                    <Image src="/assets/images/logo.png" fill alt="Logo" className="object-contain" />
+                    <Image src="/assets/images/logo.png" alt="Logo" className="object-contain" />
                 </div>
             </div>
             <div className="space-y-2 my-4">
                 <div className="flex items-center space-x-2 w-full text-[#716F78] font-medium">
                     <HiOutlineLocationMarker className="h-6 w-6 font-bold" />
-                    <h5 className="text-[15px] lg:text-xl">{location}</h5>
+                    <h5 className="text-[15px] lg:text-xl">{task.userAddress}</h5>
                 </div>
                 <div className="flex items-center space-x-2 w-full text-[#716F78] font-medium">
                     <FiCalendar className="h-6 w-6 font-bold" />
-                    <h5 className="text-[15px] lg:text-xl">{date}</h5>
+                    <h5 className="text-[15px] lg:text-xl">{formatDate(task.postedAt)}</h5>
                 </div>
                 <div className="flex items-center space-x-2 w-full text-[#716F78] font-medium">
                     <FiClock className="h-6 w-6 font-bold" />
-                    <h5 className="text-[15px] lg:text-xl">{time}</h5>
+                    <h5 className="text-[15px] lg:text-xl">{formatDate(task.postedAt)}</h5>
                 </div>
             </div>
             <div className="flex items-center justify-between">
                 <div className="flex items-center space-x-3">
-                    <h2 className="text-primary text-lg lg:text-2xl font-bold capitalize">{availabiliy}</h2>
+                    <h2 className="text-primary text-lg lg:text-2xl font-bold capitalize">{availability}</h2>
                     <div className="w-2 h-2 bg-[#716F78] rounded-full" />
-                    <h2 className="text-[#716F78] text-lg lg:text-2xl font-medium capitalize">{`5 offers`}</h2>
+                    <h2 className="text-[#716F78] text-lg lg:text-2xl font-medium capitalize">{`${task.postedAt} offers`}</h2>
                 </div>
-                <h2 className="text-primary text-2xl lg:text-[32px] font-bold capitalize">{formatAmount(customerBudget, "USD", false)}</h2>
+                <h2 className="text-primary text-2xl lg:text-[32px] font-bold capitalize">{formatAmount(task.customerBudget, "USD", false)}</h2>
             </div>
         </motion.div>
     )
 }
 
-export default TaskCard
+export default TaskCard;
--- src/components/main/explore/Tasks/index.tsx
+++ src/components/main/explore/Tasks/index.tsx
@@ -1,102 +1,11 @@
 "use client"
 
 import { useState } from "react";
-import TaskCard from "../TaskCard";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 import { useGetActiveTasksQuery } from "@/services/tasks";
 import Dropdown from "@/components/global/Dropdown";
 import { useSession } from "next-auth/react";
-
-// Sample task data
-const tasksData = {
-    content: [
-        {
-            id: 1,
-            posterId: 1,
-            taskServiceName: "Gardening",
-            category: "Home Services",
-            subCategory: "Lawn Mowing",
-            taskDescription: "Mow the lawn and trim the bushes.",
-            userAddress: "123 Green Street, Cityville",
-            postedAt: new Date("2024-05-14T22:15:51.373Z"),
-            customerBudget: 50,
-            taskImage: "http://example.com/task-image-1.jpg",
-            taskDates: [new Date("2024-05-14")],
-            active: true
-        },
-        {
-            id: 2,
-            posterId: 2,
-            taskServiceName: "Plumbing",
-            category: "Home Services",
-            subCategory: "Pipe Repair",
-            taskDescription: "Fix the leaking pipe in the kitchen.",
-            userAddress: "456 Main Street, Townville",
-            postedAt: new Date("2024-05-14T22:30:00.000Z"),
-            customerBudget: 100,
-            taskImage: "http://example.com/task-image-2.jpg",
-            taskDates: [new Date("2024-05-15")],
-            active: true
-        },
-        {
-            id: 3,
-            posterId: 3,
-            taskServiceName: "Cleaning",
-            category: "Home Services",
-            subCategory: "House Cleaning",
-            taskDescription: "Clean the entire house.",
-            userAddress: "789 Oak Street, Villageton",
-            postedAt: new Date("2024-05-15T08:00:00.000Z"),
-            customerBudget: 80,
-            taskImage: "http://example.com/task-image-3.jpg",
-            taskDates: [new Date("2024-05-16")],
-            active: true
-        },
-        {
-            id: 4,
-            posterId: 4,
-            taskServiceName: "Painting",
-            category: "Home Services",
-            subCategory: "Interior Painting",
-            taskDescription: "Paint the living room walls.",
-            userAddress: "101 Pine Street, Forestville",
-            postedAt: new Date("2024-05-15T10:00:00.000Z"),
-            customerBudget: 150,
-            taskImage: "http://example.com/task-image-4.jpg",
-            taskDates: [new Date("2024-05-17")],
-            active: true
-        },
-        {
-            id: 5,
-            posterId: 5,
-            taskServiceName: "Dog Walking",
-            category: "Pet Services",
-            subCategory: "Dog Walking",
-            taskDescription: "Walk the dog for 30 minutes.",
-            userAddress: "222 Maple Street, Groveton",
-            postedAt: new Date("2024-05-16T12:00:00.000Z"),
-            customerBudget: 20,
-            taskImage: "http://example.com/task-image-5.jpg",
-            taskDates: [new Date("2024-05-17")],
-            active: true
-        },
-        {
-            id: 6,
-            posterId: 6,
-            taskServiceName: "Tutoring",
-            category: "Education",
-            subCategory: "Math Tutoring",
-            taskDescription: "Help with algebra homework for 1 hour.",
-            userAddress: "333 Elm Street, Riverside",
-            postedAt: new Date("2024-05-16T14:00:00.000Z"),
-            customerBudget: 30,
-            taskImage: "http://example.com/task-image-6.jpg",
-            taskDates: [new Date("2024-05-18")],
-            active: true
-        }
-    ]
-};
-
+import TaskCard from "../TaskCard";
 
 const Tasks = () => {
     const [selectedFilters, setSelectedFilters] = useState({
@@ -108,12 +17,10 @@ const Tasks = () => {
     });
     const [currentPage, setCurrentPage] = useState(1);
     const itemsPerPage = 6;
+    const pageNumber = 1;
+    const { data } = useGetActiveTasksQuery(pageNumber);
 
-    const { data } = useGetActiveTasksQuery(1);
-
-    console.log("data", data)
-
-    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+    const paginate = () => setCurrentPage(data?.pageNumber!);
 
     const handleFilterChange = (filterName: string, value: string) => {
         setSelectedFilters({
@@ -123,10 +30,9 @@ const Tasks = () => {
     }
 
     const renderContent = () => {
-
         return (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-14">
-                {tasksData?.content?.map((task, index) => (
+                {data?.content?.map((task, index) => (
                     <TaskCard {...task} key={index} />
                 ))}
             </div>
@@ -144,44 +50,10 @@ const Tasks = () => {
         },
     ];
 
-    const locationDropdown = [
-        {
-            label: "Western Australia",
-            onClick: () => handleFilterChange('location', 'Western Australia')
-        },
-        {
-            label: "Northern Territory",
-            onClick: () => handleFilterChange('location', 'Northern Territory')
-        },
-        {
-            label: "South Australia",
-            onClick: () => handleFilterChange('location', 'South Australia')
-        },
-        {
-            label: "Queensland",
-            onClick: () => handleFilterChange('location', 'Queensland')
-        },
-        {
-            label: "New South Wales",
-            onClick: () => handleFilterChange('location', 'New South Wales')
-        },
-        {
-            label: "Victoria",
-            onClick: () => handleFilterChange('location', 'Victoria')
-        },
-        {
-            label: "Tasmania",
-            onClick: () => handleFilterChange('location', 'Tasmania')
-        },
-        {
-            label: "Australian Capital Territory",
-            onClick: () => handleFilterChange('location', 'Australian Capital Territory')
-        }
-    ];
 
     return (
         <section className="pt-7 container">
-            <div className="flex lg:space-x-4 lg:max-w-[810px] items-center max-lg:grid max-lg:grid-cols-2 max-lg:gap-y-8 gap-6">
+            <div className="hidden lg:flex lg:space-x-4 lg:max-w-[810px] items-center gap-6">
                 <select id="category" name="category" onChange={(e) => handleFilterChange('category', e.target.value)} className="w-full border-2 border-primary text-primary font-semibold bg-[#F1F1F2] py-2 px-4 rounded-full">
                     <option value="">Category</option>
                     <option value="Home Services">Home Services</option>
@@ -202,16 +74,7 @@ const Tasks = () => {
                 </select>
                 <select id="location" name="location" onChange={(e) => handleFilterChange('location', e.target.value)} className="w-full border-2 border-primary text-primary bg-[#F1F1F2] font-semibold py-2 px-4 rounded-full">
                     <option value="">Location</option>
-                    <option value="Western Australia">Western Australia</option>
-                    <option value="Northern Territory">Northern Territory</option>
-                    <option value="South Australia">South Australia</option>
-                    <option value="Queensland">Queensland</option>
-                    <option value="New South Wales">New South Wales</option>
-                    <option value="Victoria">Victoria</option>
-                    <option value="Tasmania">Tasmania</option>
-                    <option value="Australian Capital Territory">
-                        Australian Capital Territory
-                    </option>
+                    
                 </select>
                 <Dropdown
                     trigger={() => (
@@ -243,10 +106,32 @@ const Tasks = () => {
                     </option>
                 </select>
             </div>
+            <div className="lg:hidden flex items-center w-full justify-center">
+                <Dropdown
+                    trigger={() => (
+                        <div className="w-full border-2 border-primary flex items-center justify-center text-primary font-semibold py-2 px-4 rounded-full">
+                            Filter by
+                        </div>
+                    )}
+                    className='left-0 right-0 mx-auto top-14'>
+                    <div className='w-[240px] bg-white rounded-md flex items-center'>
+                        {typeOfServiceDropdown.map((button, index) => (
+                            <div
+                                key={index}
+                                onClick={button.onClick}
+                                className='flex w-full transition-all text-sm items-center justify-between p-3'>
+                                <div className="">
+                                    {button.label}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </Dropdown>
+            </div>
 
             <div className="">
                 {renderContent()}
-                <div className="flex items-center justify-center w-full">
+                {/* <div className="flex items-center justify-center w-full">
                     <div className="flex items-center my-10 mx-20 text-2xl border-[#D0D5DD] rounded-xl">
                         <button
                             className={`bg-primary text-white mr-10 rounded max-lg:text-xs lg:rounded-[10px] h-[21px] w-[21px] lg:h-[39px] lg:w-[39px] flex items-center justify-center ${currentPage === 1 && 'bg-status-violet'}`}
@@ -256,7 +141,7 @@ const Tasks = () => {
                             <FaChevronLeft />
                         </button>
 
-                        {tasksData && Array.from({ length: Math.ceil(tasksData.content.length / itemsPerPage) }, (_, i) => i + 1).map((number) => (
+                        {data && Array.from({ length: Math.ceil(data.content.length / itemsPerPage) }, (_, i) => i + 1).map((number) => (
                             <button
                                 key={number}
                                 className={`bg-primary text-white rounded max-lg:text-xs lg:rounded-[10px] h-[21px] w-[21px] lg:h-[39px] lg:w-[39px] flex items-center justify-center ${currentPage === number && 'bg-status-violet'}`}
@@ -267,14 +152,14 @@ const Tasks = () => {
                         ))}
 
                         <button
-                            className={`bg-primary text-white ml-10 rounded max-lg:text-xs lg:rounded-[10px] h-[21px] w-[21px] lg:h-[39px] lg:w-[39px] flex items-center justify-center ${currentPage === Math.ceil(tasksData?.content?.length! / itemsPerPage) && 'bg-status-violet'}`}
+                            className={`bg-primary text-white ml-10 rounded max-lg:text-xs lg:rounded-[10px] h-[21px] w-[21px] lg:h-[39px] lg:w-[39px] flex items-center justify-center ${currentPage === Math.ceil(data?.content?.length! / itemsPerPage) && 'bg-status-violet'}`}
                             onClick={() => paginate(currentPage + 1)}
-                            disabled={currentPage === Math.ceil(tasksData?.content?.length! / itemsPerPage)}
+                            disabled={currentPage === Math.ceil(data?.content?.length! / itemsPerPage)}
                         >
                             <FaChevronRight />
                         </button>
                     </div>
-                </div>
+                </div> */}
             </div>
         </section>
     )
