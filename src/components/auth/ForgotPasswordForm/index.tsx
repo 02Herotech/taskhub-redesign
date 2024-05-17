9f7ff437f55283f8a8ccabb1efa68ec50bd0c570
Ntami Egbe <77502425+ntamiegbe@users.noreply.github.com>
2024-05-17 09:21:46 +0100

    fix:

--- src/components/auth/ForgotPasswordForm/index.tsx
+++ src/components/auth/ForgotPasswordForm/index.tsx
@@ -42,6 +42,7 @@ const ForgotPasswordForm = () => {
             console.log(response);
 
             if (response.status == 200) {
+                toast.success(response.data.message);
                 router.push(`/auth/forgot-password/confirmation?${params}`);
                 setIsLoading(false);
             }
