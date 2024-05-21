e24a916fad16e3e67bf2bd03a9367b154cf0246f
krystein <Nworahchimaobic@gmail.com>
2024-05-21 11:08:25 +0100

    taskhub

--- src/components/global/Popup/index.tsx
+++ src/components/global/Popup/index.tsx
@@ -10,7 +10,7 @@ interface PopupProps {
 const Popup: React.FC<PopupProps> = ({ isOpen, onClose, children }) => {
     return (
         <div
-            className={`fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-60 ${isOpen ? "" : "hidden"
+            className={`fixed  left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-60 ${isOpen ? "" : "hidden"
                 }`}
         >
             <div className="relative flex justify-center rounded-lg bg-white p-2 md:p-6 shadow-lg">
