8176f203a1d0eb67514b0d41c89eff9a716a7e2d
Ntami Egbe <77502425+ntamiegbe@users.noreply.github.com>
2024-05-27 08:51:45 +0100

    fix:

--- src/components/dashboard/customer/TopActivities/index.tsx
+++ src/components/dashboard/customer/TopActivities/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TopActivities = () => {
   return (
-      <div className='flex flex-col justify-between gap-4 overflow-hidden rounded-xl bg-[#EBE9F4] p-32'>TopActivities</div>
+      <div className='flex flex-col justify-between gap-4 h-full overflow-hidden rounded-xl bg-[#EBE9F4] p-5'>TopActivities</div>
   )
 }
 
