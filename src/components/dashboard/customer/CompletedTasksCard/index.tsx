da66c626944c77fd00549d8320417576a94f7319
Ntami Egbe <77502425+ntamiegbe@users.noreply.github.com>
2024-05-28 07:07:17 +0100

    fix: completed tasks

--- src/components/dashboard/customer/CompletedTasksCard/index.tsx
+++ src/components/dashboard/customer/CompletedTasksCard/index.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import { formatAmount } from "@/lib/utils";
+import { dayOfWeekNames, formatAmount, monthNames } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 import { FiCalendar } from "react-icons/fi";
 import { HiOutlineLocationMarker } from "react-icons/hi";
 import { motion } from "framer-motion";
 import { MdCheck } from "react-icons/md";
-import Dropdown from "@/components/global/Dropdown";
+import Dropdown, { useDropdown } from "@/components/global/Dropdown";
 import { DeleteTaskSvg, DropReviewSvg, MenuDropdownSvg, ReportSvg } from "@/lib/svgIcons";
 import Button from "@/components/global/Button";
+import { useState } from "react";
+import Popup from "@/components/global/Popup";
 
 export type CompletedTasksProps = {
     taskTitle: string;
@@ -24,44 +26,47 @@ interface NewTasksProps {
 
 const CompletedTasksCard: React.FC<NewTasksProps> = ({ task }) => {
     const router = useRouter();
+    const [showReviewPopup, setShowReviewPopup] = useState(false);
 
     const date = task.time;
     const day = date.getDate();
     const suffixes = ["st", "nd", "rd", "th"];
     const daySuffix = suffixes[(day - 1) % 10] || suffixes[3];
-    const monthNames = [
-        "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December",
-    ];
-    const dayOfWeekNames = [
-        "Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat",
-    ];
     const formattedDate = `On ${dayOfWeekNames[date.getDay()]}, ${monthNames[date.getMonth()]} ${day}${daySuffix}`;
 
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const formattedTime = `${hours % 12 || 12}:${minutes < 10 ? "0" : ""}${minutes} ${hours >= 12 ? "PM" : "AM"}`;
-
-    const dropdownItems = [
-        {
-            title: 'Drop a review',
-            onClick: () => console.log('Approve'),
-            icon: DropReviewSvg
-        },
-        {
-            title: 'Report',
-            onClick: () => console.log('Request Revision'),
-            icon: ReportSvg
-        },
-        {
-            title: 'Delete Task',
-            onClick: () => console.log('Delete'),
-            icon: DeleteTaskSvg
-        }
-    ];
+    const DropdownItem = ({ title, onClick, icon }: any) => {
+        const { closeDropdown } = useDropdown();
+        return (
+            <button
+                className="dropdown-item flex items-center space-x-3"
+                onClick={(event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    onClick();
+                    closeDropdown();
+                }}
+            >
+                <div className="bg-status-darkViolet w-9 h-9 flex items-center justify-center rounded-full">{icon}</div>
+                <h5 className="text-lg text-status-darkViolet font-satoshiMedium">{title}</h5>
+            </button>
+        );
+    };
 
     return (
         <div>
+            {showReviewPopup && (
+                <Popup isOpen={showReviewPopup} onClose={() => setShowReviewPopup(false)}>
+                    <div className="relative font-satoshi rounded-xl h-[312px] bg-[#EBE9F4] max-lg:mx-10 lg:w-[577px]">
+                        <div className="border-b border-primary p-4 flex items-center space-x-3">
+                            <div className="bg-status-darkViolet w-9 h-9 flex items-center justify-center rounded-full">{DropReviewSvg}</div>
+                            <h2 className="text-[#2A1769] font-bold text-3xl">Drop a review</h2>
+                        </div>
+                        <div className="p-4">
+                            He/She 
+                        </div>
+                    </div>
+                </Popup>
+            )}
             <motion.div
                 className="lg:rounded-4xl font-satoshi rounded-xl bg-[#EBE9F4] p-5 mb-4 flex justify-between"
                 whileHover={{
@@ -96,23 +101,28 @@ const CompletedTasksCard: React.FC<NewTasksProps> = ({ task }) => {
                         </div>
                     ) : (
                         <div className="">
-                            <Dropdown
-                                trigger={
-                                    () => (
-                                        <button>
-                                            {MenuDropdownSvg}
-                                        </button>
-                                    )}
-                                className='right-0 mx-auto top-12'>
-                                <form className='bg-[#EBE9F4] border border-primary rounded-md w-[218px] py-4 px-2 space-y-3'>
-                                    {dropdownItems.map((item, index) => (
-                                        <button className="flex items-center space-x-3" key={index} onClick={item.onClick}>
-                                            <div className="bg-status-darkViolet w-9 h-9 flex items-center justify-center rounded-full">{item.icon}</div>
-                                            <h5 className="text-lg text-status-darkViolet font-satoshiMedium">{item.title}</h5>
-                                        </button>
-                                    ))}
-                                </form>
-                            </Dropdown>
+                                <Dropdown
+                                    trigger={() => <button>{MenuDropdownSvg}</button>}
+                                    className="right-0 mx-auto top-12"
+                                >
+                                    <div className="bg-[#EBE9F4] border border-primary rounded-md w-[218px] py-4 px-2 space-y-3">
+                                        <DropdownItem
+                                            title="Drop a review"
+                                            onClick={() => setShowReviewPopup(true)}
+                                            icon={DropReviewSvg}
+                                        />
+                                        <DropdownItem
+                                            title="Report"
+                                            onClick={() => console.log("Request Revision")}
+                                            icon={ReportSvg}
+                                        />
+                                        <DropdownItem
+                                            title="Delete Task"
+                                            onClick={() => console.log("Delete")}
+                                            icon={DeleteTaskSvg}
+                                        />
+                                    </div>
+                                </Dropdown>
                         </div>
                     )}
 
