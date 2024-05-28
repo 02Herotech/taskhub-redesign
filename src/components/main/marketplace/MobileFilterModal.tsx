bb60e5af3456c6fa41fc926e2e0cccc9fe7b2db6
BashBuilder <iamadelowotan@gmail.com>
2024-05-28 06:59:51 +0100

    fix: bug fixed

--- src/components/main/marketplace/MobileFilterModal.tsx
+++ src/components/main/marketplace/MobileFilterModal.tsx
@@ -88,6 +88,7 @@ const MobileFilterModal = ({
       }),
     );
   };
+
   return (
     <section
       className={`fixed left-0 top-0 z-40 flex h-screen w-screen items-center justify-center bg-black bg-opacity-70 transition-all duration-300 ${isMobileFilterModalShown ? "pointer-events-auto opacity-100 " : " pointer-events-none opacity-0"} `}
