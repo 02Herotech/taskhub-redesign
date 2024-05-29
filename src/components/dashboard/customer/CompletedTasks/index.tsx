c4286d20e57152fa5779c2943828bda55b304712
Ntami Egbe <77502425+ntamiegbe@users.noreply.github.com>
2024-05-29 11:49:24 +0100

    fix:

--- src/components/dashboard/customer/CompletedTasks/index.tsx
+++ src/components/dashboard/customer/CompletedTasks/index.tsx
@@ -4,8 +4,6 @@ import React from "react";
 import { useGetCustomerCompletedTasksQuery } from "@/services/tasks";
 import { useSession } from "next-auth/react";
 import Loading from "@/shared/loading";
-import Link from "next/link";
-import Button from "@/components/global/Button";
 import CompletedTasksCard from "../CompletedTasksCard";
 
 const TaskList: React.FC = () => {
@@ -22,16 +20,13 @@ const TaskList: React.FC = () => {
 
     return (
         <>
-            {!tasksData?.content?.length && (
+            {!tasksData?.length && (
                 <div className="flex flex-col items-center justify-center space-y-5 h-[50vh]">
                     <h2 className="text-2xl font-bold text-primary text-center">No tasks completed</h2>
-                    {/* <Link href="/customer/add-task">
-                        <Button className="rounded-full">Add new task</Button>
-                    </Link> */}
                 </div>
             )}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                {tasksData?.content?.map((task, index) => (
+                {tasksData?.map((task, index) => (
                     <CompletedTasksCard key={index} task={task} />
                 ))}
             </div>
