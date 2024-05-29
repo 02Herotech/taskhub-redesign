187cc0c2237a7d22eaea7a59f7e4593f83ada770
Ntami Egbe <77502425+ntamiegbe@users.noreply.github.com>
2024-05-29 23:36:50 +0100

    fix:

--- src/app/(dashboard)/service-provider/jobs/page.tsx
+++ src/app/(dashboard)/service-provider/jobs/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-export const JobsData = [
+const JobsData = [
   {
     id: "1",
     name: "Kelly Jane",
