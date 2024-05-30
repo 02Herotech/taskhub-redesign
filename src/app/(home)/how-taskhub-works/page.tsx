33344000d6c259fc36ad1faf6321c1e1aa5ea43f
unknown <oguntayomicheal@gmail.com>
2024-05-30 08:47:47 +0100

    Fix deployment error

--- src/app/(home)/how-taskhub-works/page.tsx
+++ src/app/(home)/how-taskhub-works/page.tsx
@@ -1,10 +1,8 @@
 
 'use client'
 import Header from '@/components/HowTaskhubWorks/Header'
-import PostTasks from '@/components/HowTaskhubWorks/PostTasks'
 import React, { useEffect } from 'react'
-import PostService from '@/components/HowTaskhubWorks/PostService'
-import Experiences from '@/components/HowTaskhubWorks/Experience/indes'
+
 import { useRouter } from 'next/navigation'
 const HowTaskhubWorks = () => {
     const router = useRouter()
@@ -13,15 +11,6 @@ const HowTaskhubWorks = () => {
     })
 
     return (
-        // <div className='py-10 bg-white lg:bg-gradient-to-b from-[#f3dcfc]  via-[#FFFFFF] to-[#F5DDFD]'>
-
-        //     <div className='mx-auto max-w-7xl xl:mx-[120px] lg:mx-[48px] m-[20px] my-0'>
-
-        //         <PostTasks />
-        //         <PostService/>
-        //         <Experiences/>
-        //     </div>
-        // </div>
         <span></span>
     )
 }
