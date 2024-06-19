c611a9e784500753fd77c7cd543cf7d6a41ad680
BashBuilder <iamadelowotan@gmail.com>
2024-06-19 20:15:30 +0100

    update

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,151 +1,147 @@
 type Link = {
+  label: string;
+  url?: string;
+  sublinks?: {
     label: string;
-    url?: string;
+    url: string;
     sublinks?: {
-        label: string;
-        url: string;
-        sublinks?: {
-            label: string;
-            url: string;
-        }[];
+      label: string;
+      url: string;
     }[];
+  }[];
 };
 
 export const customerLinks: Link[] = [
-    {
-        label: "Add a task",
-        url: "/customer/add-task",
-    },
-    {
-        label: "Marketplace",
-        url: "/marketplace",
-    },
-    {
-        label: "Profile",
-        url: "/customer/profile"
-    }
+  {
+    label: "Add a task",
+    url: "/customer/add-task",
+  },
+  {
+    label: "Marketplace",
+    url: "/marketplace",
+  },
+  {
+    label: "Profile",
+    url: "/customer/profile",
+  },
 ];
 
 export const serviceProviderLinks: Link[] = [
-    {
-        label: "Provide a service",
-        url: "/provide-service",
-    },
-    {
-        label: "Explore Tasks",
-        url: "/explore",
-    },
-    {
-        label: "Marketplace",
-        url: "/marketplace",
-    },
+  {
+    label: "Provide a service",
+    url: "/provide-service",
+  },
+  {
+    label: "Explore Tasks",
+    url: "/explore",
+  },
+  {
+    label: "Marketplace",
+    url: "/marketplace",
+  },
 ];
 
 export const homeLinks: Link[] = [
-    {
-        label: "Home",
-        url: "/",
-    },
-    {
-        label: "About",
-        url: "/about",
-    },
-    {
-        label: "Marketplace",
-        url: "/marketplace",
-    },
-    {
-        label: "Contact Us",
-        url: "/contact",
-    },
+  {
+    label: "Home",
+    url: "/",
+  },
+  {
+    label: "About",
+    url: "/about",
+  },
+  {
+    label: "Marketplace",
+    url: "/marketplace",
+  },
+  {
+    label: "Contact Us",
+    url: "/contact",
+  },
 ];
 
 export const mobileServiceProviderLinks: Link[] = [
-    {
-        label: "Provide a service",
-        url: "/provide-service",
-    },
-    {
-        label: "Explore Tasks",
-        url: "/explore",
-    },
-    {
-        label: "Marketplace",
-        url: "/marketplace",
-    },
-    {
-        label: "Profile",
+  {
+    label: "Provide a service",
+    url: "/provide-service",
+  },
+  {
+    label: "Explore Tasks",
+    url: "/explore",
+  },
+  {
+    label: "Marketplace",
+    url: "/marketplace",
+  },
+  {
+    label: "Profile",
+    sublinks: [
+      {
+        label: "My Profile",
+        url: "/service-provider/profile",
+      },
+      {
+        label: "My Services",
+        url: "/service-provider/services",
+      },
+      {
+        label: "My Notifications",
+        url: "/service-provider/notification",
+      },
+      {
+        label: "Payments and wallet",
+        url: "/service-provider/payment",
+      },
+      {
+        label: "Settings",
+        url: "/service-provider/settings",
         sublinks: [
-            {
-                label: "My Profile",
-                url: "/service-provider/dashboard",
-            },
-            {
-                label: "My Services",
-                url: "/service-provider/dashboard/services",
-            },
-            {
-                label: "My Notifications",
-                url: "/service-provider/notification",
-            },
-            {
-                label: "Payments and wallet",
-                url: "/service-provider/dashboard/payment",
-            },
-            {
-                label: "Settings",
-                url: "/service-provider/dashbaord/settings",
-                sublinks: [
-                    {
-                        label: "General",
-                        url: "/service-provider/dashboard/settings/general",
-                    },
-                    {
-                        label: "Security",
-                        url: "/service-provider/dashboard/settings/security",
-                    },
-                    {
-                        label: "Privacy",
-                        url: "/service-provider/dashboard/settings/privacy",
-                    },
-                ]
-            },
-        ]
-    }
+          {
+            label: "Password",
+            url: "/service-provider/dashboard/settings/security",
+          },
+          {
+            label: "notification",
+            url: "/service-provider/dashboard/settings/privacy",
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 export const mobileCustomerLinks: Link[] = [
-    {
-        label: "Add a task",
-        url: "/customer/add-task",
-    },
-    {
-        label: "Marketplace",
-        url: "/marketplace",
-    },
-    {
-        label: "Profile",
-        sublinks: [
-            {
-                label: "My Profile",
-                url: "/customer/profile",
-            },
-            {
-                label: "My Tasks",
-                url: "/customer/tasks",
-            },
-            {
-                label: "My Notifications",
-                url: "/customer/notifications",
-            },
-            {
-                label: "Payments settings",
-                url: "/customer/payment/payment-history",
-            },
-            {
-                label: "Settings",
-                url: "/customer/settings",
-            },
-        ]
-    }
+  {
+    label: "Add a task",
+    url: "/customer/add-task",
+  },
+  {
+    label: "Marketplace",
+    url: "/marketplace",
+  },
+  {
+    label: "Profile",
+    sublinks: [
+      {
+        label: "My Profile",
+        url: "/customer/profile",
+      },
+      {
+        label: "My Tasks",
+        url: "/customer/tasks",
+      },
+      {
+        label: "My Notifications",
+        url: "/customer/notifications",
+      },
+      {
+        label: "Payments settings",
+        url: "/customer/payment/payment-history",
+      },
+      {
+        label: "Settings",
+        url: "/customer/settings",
+      },
+    ],
+  },
 ];
