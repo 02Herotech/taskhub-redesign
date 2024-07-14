98459d59d541dca8488e600dc993db7e2e928f02
BashBuilder <iamadelowotan@gmail.com>
2024-07-14 09:56:18 +0100

    profile percentage optimized

--- src/components/serviceProviderDashboard/profile/ProfilePieChart.tsx
+++ src/components/serviceProviderDashboard/profile/ProfilePieChart.tsx
@@ -12,10 +12,10 @@ interface ProfilePieChartPropType {
 
 const ProfilePieChart = ({ chartData }: ProfilePieChartPropType) => {
   const data = {
-    labels: ["Complete", "Incomplete"],
+    // labels: ["Complete", "Incomplete"],
     datasets: [
       {
-        label: "Completion Rate",
+        // label: "Completion Rate",
         data: [chartData.completed, chartData.total - chartData.completed],
         backgroundColor: ["#381F8C", "#fff"],
       },
@@ -24,7 +24,7 @@ const ProfilePieChart = ({ chartData }: ProfilePieChartPropType) => {
   return (
     <div className="relative">
       <p className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-[calc(50%+0.5rem)] text-xl font-bold text-orange-normal">
-        {Math.floor((chartData.completed / chartData.total) * 100)}
+        {Math.floor((chartData.completed / chartData.total) * 100)}%
       </p>
       <Pie data={data} />
     </div>
