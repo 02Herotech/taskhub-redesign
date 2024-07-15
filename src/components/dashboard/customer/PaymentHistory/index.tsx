b91ff0563da524d81ce28a56c0b2a0e59d92e19e
Ntami Egbe <77502425+ntamiegbe@users.noreply.github.com>
2024-07-15 06:21:36 +0100

    feat: implemented filter functionality for offers and payment history

--- src/components/dashboard/customer/PaymentHistory/index.tsx
+++ src/components/dashboard/customer/PaymentHistory/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useRef, useState } from 'react';
-import { formatAmount } from "@/lib/utils";
+import { formatAmount, getYesterday } from "@/lib/utils";
 import Button from "@/components/global/Button";
 import Popup from '@/components/global/Popup';
 import { FiClock } from 'react-icons/fi';
@@ -12,12 +12,16 @@ import { Receipt } from '@/types/services/invoice';
 import Loading from '@/components/global/loading/page';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { LuCalendarDays } from "react-icons/lu";
 
 const PaymentHistory = () => {
     const [visibleTransactions, setVisibleTransactions] = useState(4);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedPayment, setSelectedPayment] = useState<Receipt | null>(null);
-    const todayDate = new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+    const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
     const { profile: user } = useSelector(
         (state: RootState) => state.userProfile,
     );
@@ -78,9 +82,23 @@ const PaymentHistory = () => {
         });
     };
 
-    const groupReceiptsByDate = (receipts: Receipt[]) => {
+    const groupReceiptsByDate = (receipts: Receipt[], filterDate: Date | null) => {
+        if (!receipts || !Array.isArray(receipts)) {
+            return [];
+        }
+
         const grouped = receipts.reduce((acc, receipt) => {
-            const date = formatCreatedAt(receipt.createdAt);
+            const date = new Date(receipt.createdAt[0], receipt.createdAt[1] - 1, receipt.createdAt[2]).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+            });
+            if (filterDate) {
+                const receiptDate = new Date(receipt.createdAt[0], receipt.createdAt[1] - 1, receipt.createdAt[2]);
+                if (receiptDate.toDateString() !== filterDate.toDateString()) {
+                    return acc;
+                }
+            }
             if (!acc[date]) {
                 acc[date] = [];
             }
@@ -93,44 +111,91 @@ const PaymentHistory = () => {
         );
     };
 
-    const groupedReceipts = groupReceiptsByDate(paymentHistoryData);
+    const groupedReceipts = groupReceiptsByDate(paymentHistoryData, selectedDate);
     const dateArray = selectedPayment?.createdAt;
     const formattedDate: string = dateArray ? new Date(Number(dateArray[0]), Number(dateArray[1]) - 1, Number(dateArray[2])).toLocaleDateString() : '';
 
     return (
         <div className="w-full bg-[#EBE9F4] rounded-[20px] p-4 font-satoshi">
-            {paymentHistoryData.length === 0 && (
-                <div className="flex flex-col items-center justify-center space-y-5 h-[50vh]">
-                    <h2 className="text-2xl font-bold text-primary text-center">No payment history found</h2>
-                </div>
-            )}
-            {groupedReceipts.slice(0, visibleTransactions).map(([date, dateReceipts]) => (
-                <div key={date} className="mb-8">
-                    <h3 className="text-[#140B31] font-satoshiBold font-bold text-base mb-5">{date}</h3>
-                    <div className="space-y-5 cursor-pointer">
-                        {dateReceipts.map((payment, index) => (
-                            <div
-                                key={index}
-                                className="flex flex-col lg:flex-row items-start lg:items-center justify-between lg:px-5 py-3 border-b border-primary"
-                                onClick={() => handleCardClick(payment)}
-                            >
-                                <div className="flex items-center w-full space-x-5 mb-3 lg:mb-0">
-                                    <div className="w-14 h-14 bg-[#C1BADB] rounded-full flex items-center justify-center">
-                                        <div className="w-7 h-7 bg-white rounded-full" />
-                                    </div>
-                                    <div className="flex-grow">
-                                        <h4 className="text-primary font-bold text-xl mb-1">{payment.bookingTitle}</h4>
-                                        <p className="text-[#716F78] font-satoshiMedium text-base">{payment.bookingCategory}</p>
-                                    </div>
-                                </div>
-                                <h2 className="text-xl font-bold capitalize text-tc-orange lg:text-[22px] lg:mt-0 mt-2">
-                                    AUD{formatAmount(payment.total, "USD", false)}
-                                </h2>
-                            </div>
-                        ))}
+            <div className="flex items-center justify-end mb-4">
+                <div className="relative flex items-center">
+                    {selectedDate && (
+                        <Button
+                            onClick={() => setSelectedDate(null)}
+                            className="font-bold rounded-full mr-3"
+                            theme="outline"
+                            size="sm"
+                        >
+                            View all
+                        </Button>
+                    )}
+                    <div className="bg-primary text-white p-[6px] rounded-full cursor-pointer flex items-center justify-center hover:scale-105 transition-all" onClick={() => setIsDatePickerOpen(!isDatePickerOpen)}>
+                        <LuCalendarDays
+                            size={20}
+                        />
                     </div>
+                    {isDatePickerOpen && (
+                        <DatePicker
+                            selected={selectedDate}
+                            onChange={(date: Date) => {
+                                setSelectedDate(date);
+                                setIsDatePickerOpen(false);
+                            }}
+                            onClickOutside={() => setIsDatePickerOpen(false)}
+                            maxDate={getYesterday()}
+                            inline
+                            className="absolute right-0 top-8 z-10"
+                            renderDayContents={(day, date) => {
+                                return (
+                                    <div title={date! > getYesterday() ? "Only past dates can be selected" : undefined}>
+                                        {day}
+                                    </div>
+                                );
+                            }}
+                        />
+                    )}
                 </div>
-            ))}
+            </div>
+            {groupedReceipts.length === 0 ? (
+                <div className="flex flex-col items-center justify-center space-y-5 h-[50vh]">
+                    <h2 className="text-2xl font-bold text-primary text-center">
+                        {selectedDate
+                            ? `No payment history found for ${selectedDate.toLocaleDateString("en-US", {
+                                year: "numeric",
+                                month: "long",
+                                day: "numeric",
+                            })}`
+                            : "No payment history found"}
+                    </h2>
+                </div>
+            ) : (
+                groupedReceipts.slice(0, visibleTransactions).map(([date, dateReceipts]) => (
+                    <div key={date} className="mb-8">
+                        <h3 className="text-[#140B31] font-satoshiBold font-bold text-base mb-5">{date}</h3>
+                        <div className="space-y-5 cursor-pointer">
+                            {dateReceipts.map((payment, index) => (
+                                <div
+                                    key={index}
+                                    className="flex flex-col lg:flex-row items-start lg:items-center justify-between lg:px-5 py-3 border-b border-primary"
+                                    onClick={() => handleCardClick(payment)}
+                                >
+                                    <div className="flex items-center w-full space-x-5 mb-3 lg:mb-0">
+                                        <div className="w-14 h-14 bg-[#C1BADB] rounded-full flex items-center justify-center">
+                                            <div className="w-7 h-7 bg-white rounded-full" />
+                                        </div>
+                                        <div className="flex-grow">
+                                            <h4 className="text-primary font-bold text-xl mb-1">{payment.bookingTitle}</h4>
+                                            <p className="text-[#716F78] font-satoshiMedium text-base">{payment.bookingCategory}</p>
+                                        </div>
+                                    </div>
+                                    <h2 className="text-xl font-bold capitalize text-tc-orange lg:text-[22px] lg:mt-0 mt-2">
+                                        AUD{formatAmount(payment.total, "USD", false)}
+                                    </h2>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )))}
             {visibleTransactions < groupedReceipts.length && (
                 <div className="flex items-center justify-center">
                     <Button onClick={handleLoadMore} className='rounded-full flex items-center space-x-2'>
@@ -139,7 +204,7 @@ const PaymentHistory = () => {
                     </Button>
                 </div>
             )}
-            
+
             {isModalOpen && selectedPayment && (
                 <Popup isOpen={isModalOpen} onClose={closeModal}>
                     <div className="bg-[#EBE9F4] rounded-2xl">
