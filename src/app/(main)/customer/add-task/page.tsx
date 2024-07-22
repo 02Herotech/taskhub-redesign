20bbc55d02b826f7fee3d33dc50a299d79c5e8fa
krystein <Nworahchimaobic@gmail.com>
2024-07-22 15:37:54 +0100

    taskhub

--- src/app/(main)/customer/add-task/page.tsx
+++ src/app/(main)/customer/add-task/page.tsx
@@ -369,7 +369,7 @@ const AddTaskForm: React.FC = () => {
       const day = String(date.getDate()).padStart(2, "0");
       const month = String(date.getMonth() + 1).padStart(2, "0");
       const year = date.getFullYear();
-      return `${day}-${month}-${year}`;
+      return `${year}-${month}-${day}`;
     }
     return "";
   };
