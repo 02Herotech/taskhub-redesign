322302a54bc2619a5e9742f3cff40baae680b467
Ntami Egbe <77502425+ntamiegbe@users.noreply.github.com>
2024-09-23 05:56:30 +0100

    fix: edit profile

--- src/components/dashboard/general/NotificationSettings.tsx
+++ src/components/dashboard/general/NotificationSettings.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import Button from "@/components/global/Button";
@@ -20,8 +19,11 @@ const NotificationsSettings = () => {
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
   const [pageLoading, setPageLoading] = useState(false);
-  const [notificationPreferences, setNotificationPreferences] = useState<string[]>([]);
-  const [fetchedNotificationPreferences, setFetchedNotificationPreferences] = useState<string[]>([]);
+  const [notificationPreferences, setNotificationPreferences] = useState<
+    string[]
+  >([]);
+  const [fetchedNotificationPreferences, setFetchedNotificationPreferences] =
+    useState<string[]>([]);
 
   const isServiceProvider = auth?.role?.[0] === "SERVICE_PROVIDER";
 
@@ -31,7 +33,7 @@ const NotificationsSettings = () => {
       value: "BOOKING",
     },
     {
-      label: "When someone sends me an offer",
+      label: "When someone send me an offer",
       value: "INVOICE",
     },
     {
@@ -47,9 +49,26 @@ const NotificationsSettings = () => {
       value: "PAYMENT",
     },
   ];
-  
 
-  // Fetch current notification preferences
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!auth || !user) return;
+    try {
+      setLoading(true);
+      const url = `https://smp.jacinthsolutions.com.au/api/v1/notification/preference?userId=${user.id}`;
+      await axios.post(url, notificationPreferences, {
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
+        },
+      });
+      setSuccess(true);
+    } catch (error: any) {
+      console.log(error?.response?.data || error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const handleGetPreferences = async () => {
     if (!auth || !user) return;
     setPageLoading(true);
@@ -60,48 +79,18 @@ const NotificationsSettings = () => {
           Authorization: `Bearer ${auth.token}`,
         },
       });
-      setFetchedNotificationPreferences(data);
-      console.log(data)
       setNotificationPreferences(data);
     } catch (error: any) {
-      console.error(error?.response?.data || error);
+      console.log(error?.response?.data || error);
     } finally {
       setPageLoading(false);
     }
   };
 
   useEffect(() => {
-    if (auth && user) {
-      handleGetPreferences();
-    }
+    handleGetPreferences();
+    // eslint-disable-next-line
   }, [auth, user]);
-
-  // Handle form submission
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (!auth || !user) return;
-    try {
-      setLoading(true);
-      const url = `https://smp.jacinthsolutions.com.au/api/v1/notification/preference?userId=${user.id}`;
-      await axios.post(url, {
-        headers: {
-          Authorization: `Bearer ${auth.token}`,
-        },
-      });
-      setSuccess(true);
-    } catch (error: any) {
-      console.error(error?.response?.data || error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // Handle checkbox changes
-  const handleCheckboxChange = (value: string, checked: boolean) => {
-    setNotificationPreferences((prev) =>
-      checked ? [...prev, value] : prev.filter((item) => item !== value),
-    );
-  };
 
   return (
     <>
@@ -118,9 +107,9 @@ const NotificationsSettings = () => {
                 onClick={() => setSuccess(false)}
               />
               <div className="relative z-10 flex w-[90vw] max-w-xl flex-col items-center justify-center gap-3 rounded-xl bg-white p-3 px-4 lg:space-y-4 lg:p-10">
-                <div className="flex flex-col items-center justify-center gap-4">
+                <div className=" flex flex-col items-center justify-center gap-4">
                   <div className="flex size-20 items-center justify-center rounded-full bg-[#C1F6C3] bg-opacity-60">
-                    <div className="flex size-14 items-center justify-center rounded-full bg-[#A6F8AA] p-2">
+                    <div className=" flex size-14 items-center justify-center rounded-full bg-[#A6F8AA] p-2">
                       <PiSealCheckFill className="size-10 text-green-500" />
                     </div>
                   </div>
@@ -128,7 +117,7 @@ const NotificationsSettings = () => {
                     Success
                   </p>
                   <p className="text-center font-semibold text-violet-darker">
-                    Your Notification preferences have been successfully updated.
+                    Your Notification preference has succesfully
                   </p>
                   <div className="flex items-center gap-6">
                     <Link
@@ -146,7 +135,6 @@ const NotificationsSettings = () => {
               </div>
             </section>
           )}
-
           <div className="flex items-center justify-between rounded-2xl bg-[#EBE9F4] px-6 py-3 lg:px-8 lg:py-4">
             <div className="space-y-3">
               <h3 className="font-satoshiBold text-xl font-bold text-[#140B31] lg:text-2xl">
@@ -160,7 +148,6 @@ const NotificationsSettings = () => {
               <IoIosNotificationsOutline className="size-5 lg:size-8" />
             </div>
           </div>
-
           <form
             onSubmit={handleSubmit}
             className="mt-5 rounded-2xl bg-[#EBE9F4] px-6 py-3 lg:px-8 lg:py-4"
@@ -179,19 +166,28 @@ const NotificationsSettings = () => {
                     value={option.value}
                     checked={notificationPreferences.includes(option.value)}
                     onChange={(event) =>
-                      handleCheckboxChange(option.value, event.target.checked)
+                      setNotificationPreferences((prev) =>
+                        event.target.checked
+                          ? [...(prev as string[]), option.value]
+                          : (prev.filter(
+                            (item) => item !== option.value,
+                          ) as string[]),
+                      )
                     }
                     className="h-4 w-4 cursor-pointer lg:h-5 lg:w-5"
                   />
                 </div>
               ))}
-
             <div className="mt-6 flex items-center justify-center lg:justify-end">
               <button
                 className="rounded-full bg-violet-normal px-4 py-2 font-bold text-white lg:w-48"
                 disabled={loading}
               >
-                {loading ? <BeatLoader color="white" loading={loading} /> : "Save"}
+                {loading ? (
+                  <BeatLoader color="white" loading={loading} />
+                ) : (
+                  "Save"
+                )}
               </button>
             </div>
           </form>
