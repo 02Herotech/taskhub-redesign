16f4e2ba96c74df4509187a0de4521979ddad675
Ntami Egbe <77502425+ntamiegbe@users.noreply.github.com>
2024-09-30 10:10:43 +0100

    fix: contact us and navbar

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -2,12 +2,14 @@ import { FiLogOut } from "react-icons/fi";
 import {
   AboutUsSvg,
   BlogSvg,
+  BusinessHubSvg,
   ContactUsSvg,
   DashboardSvg,
   HomeSvg,
   LoginSvg,
   MarketPlaceSvg,
   MessagesSvg,
+  RentShopSvg,
 } from "./svgIcons";
 import React from "react";
 
@@ -42,19 +44,19 @@ export const homeMobileLinks: LinkRouteTypes[] = [
     icon: LoginSvg,
   },
   {
-    label: "Blog",
-    url: "/blog",
-    icon: BlogSvg,
-  },
-  {
     label: "About us",
     url: "/about",
     icon: AboutUsSvg,
   },
   {
-    label: "Contact Us",
-    url: "/contact",
-    icon: ContactUsSvg,
+    label: "Business Hub",
+    url: "/coming-soon",
+    icon: BusinessHubSvg,
+  },
+  {
+    label: "Rent a shop",
+    url: "/coming-soon",
+    icon: RentShopSvg,
   },
 ];
 
