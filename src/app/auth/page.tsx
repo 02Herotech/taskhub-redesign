ff3c1f03ff92921f27c6d2e4b81058b03b76981c
System3-2 <olojam4969@gmail.com>
2025-01-30 20:45:28 +0700

    Fixed onboarding on auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -5,35 +5,41 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const AuthForm = () => {
-    const [userType, setUserType] = useState<"Customer" | "Service Provider">("Customer");
+    const [userType, setUserType] = useState<"Customer" | "Service Provider">(
+        "Customer",
+    );
     const router = useRouter();
 
     const params = new URLSearchParams({ userType });
 
     return (
-        <section className='w-full xl:w-[554px] h-full flex items-center justify-center mx-auto p-5 lg:p-10'>
-            <div className='space-y-10'>
-                <div className="space-y-4 font-clashDisplay">
-                    <h1 className='text-2xl lg:text-4xl text-[#190E3F] font-medium'>
+        <section className="mx-auto flex h-full w-full items-center justify-center p-5 lg:p-10 xl:w-[554px]">
+            <div className="space-y-10">
+                <div className="font-clashDisplay space-y-4">
+                    <h1 className="text-2xl font-medium text-[#190E3F] lg:text-4xl">
                         Create Account
                     </h1>
-                    <p className='text-xl lg:text-2xl text-tc-gray font-medium'>
+                    <p className="text-xl font-medium text-tc-gray lg:text-2xl">
                         Join us for exclusive access to our services
                     </p>
                 </div>
 
                 <div className="space-y-10 font-satoshi">
-                    <h2 className="text-primary lg:text-2xl font-bold">Sign up as a:</h2>
-                    <div className='flex items-center max-lg:justify-center space-x-8 w-full'>
+                    <h2 className="font-bold text-primary lg:text-2xl">Sign up as a:</h2>
+                    <div className="flex w-full items-center space-x-8 max-lg:justify-center">
                         <button
                             onClick={() => setUserType("Customer")}
-                            className={`h-[48px] w-[120px] lg:w-[210px] lg:h-[70px] bg-status-lightViolet rounded-2xl font-bold text-sm lg:text-2xl text-primary ${userType === "Customer" ? "border border-primary" : "border-none"}`}
+                            className={`h-[48px] w-[120px] lg:h-[70px] lg:w-[210px] 
+  ${userType === "Customer" ? "border border-primary bg-primary text-white" : "border-none bg-status-lightViolet text-primary"} 
+  rounded-2xl text-sm font-bold lg:text-2xl`}
                         >
                             Customer
                         </button>
                         <button
                             onClick={() => setUserType("Service Provider")}
-                            className={`h-[48px] w-[120px] lg:w-[210px] lg:h-[70px] bg-status-lightViolet rounded-2xl font-bold text-sm lg:text-2xl text-primary ${userType === "Service Provider" ? "border border-primary" : "border-none"}`}
+                            className={`h-[48px] w-[120px] lg:h-[70px] lg:w-[210px] 
+  ${userType === "Service Provider" ? "border border-primary bg-primary text-white" : "border-none bg-status-lightViolet text-primary"} 
+  rounded-2xl text-sm font-bold lg:text-2xl`}
                         >
                             Service Provider
                         </button>
@@ -41,16 +47,19 @@ const AuthForm = () => {
 
                     <div className="max-lg:flex max-lg:items-center max-lg:justify-center">
                         <button
-                            className='w-[170px] h-[40px] rounded-full font-normal bg-primary text-white'
+                            className="h-[40px] w-[170px] rounded-full bg-primary font-normal text-white"
                             onClick={() => router.push(`/auth/sign-up?${params.toString()}`)}
                         >
                             Next
                         </button>
                     </div>
 
-                    <h3 className="text-xl font-bold max-lg:text-center text-[#190E3F]">
+                    <h3 className="text-xl font-bold text-[#190E3F] max-lg:text-center">
                         Have an existing account?
-                        <Link href="/auth/login" className="text-primary"> Login</Link>
+                        <Link href="/auth/login" className="text-primary">
+                            {" "}
+                            Login
+                        </Link>
                     </h3>
                 </div>
             </div>
