08a8629e959de13d6b836fa82f7c655b7393bfb0
damzymichael <105460672+damzymichael@users.noreply.github.com>
2025-02-05 15:10:59 +0100

    Remove go to profile button from in review popup

--- src/app/(main)/explore/task-details/[id]/page.tsx
+++ src/app/(main)/explore/task-details/[id]/page.tsx
@@ -30,6 +30,8 @@ import { ShareSvg } from "@/lib/svgIcons";
 import useUserProfileData from "@/hooks/useUserProfileData";
 import Popup from "@/components/global/Popup";
 import PopupTwo from "@/components/global/Popup/PopupTwo";
+import ProfileIncomplete from "@/components/global/Popup/ProfileIncomplete";
+import InReview from "@/components/global/Popup/InReview";
 
 const TaskDetailsPage = ({ params }: { params: { id: string } }) => {
   const [offerAmount, setOfferAmount] = useState("");
@@ -45,7 +47,7 @@ const TaskDetailsPage = ({ params }: { params: { id: string } }) => {
   const pathname = usePathname();
   const router = useRouter();
   const fetchedUserData = useUserProfileData();
-  
+
   const [shareDropdownOpen, setShareDropdownOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
@@ -521,97 +523,20 @@ const TaskDetailsPage = ({ params }: { params: { id: string } }) => {
           taskId={Number(id)}
         />
       )}
-      <PopupTwo
+      <ProfileIncomplete
         isOpen={
           showErrorPopup &&
           (fetchedUserData?.verificationStatus === null ||
             fetchedUserData?.verificationStatus === "NOT_VERIFIED")
         }
         onClose={() => setShowErrorPopup(false)}
-      >
-        <div className="relative max-h-[700px] min-w-[320px] max-w-[700px] bg-white p-5 sm:min-w-[560px]">
-          <h3 className="mb-7 mt-4 text-center font-clashSemiBold text-2xl text-[#2A1769] sm:text-4xl">
-            Profile Update Needed.
-          </h3>
-          <p className="mx-auto mb-10 max-w-[383px] text-center font-satoshiMedium text-base text-[#140B31] sm:text-lg md:text-xl">
-            Level up your profile and unlock new opportunities! ✨ Update your
-            profile to connect with clients, make offers and start getting paid
-          </p>
-          <div className="flex justify-center gap-5">
-            <button
-              onClick={() => setShowErrorPopup(false)}
-              className="rounded-full border-[0.5px] border-primary bg-[#EBE9F4] px-5 py-2 font-bold text-primary"
-            >
-              Back
-            </button>
-            <Link
-              href="/service-provider/profile"
-              className="rounded-full bg-[#381F8C] px-5 py-2 font-bold text-[#EBE9F4]"
-            >
-              Go To Profile
-            </Link>
-          </div>
-          <Image
-            src="/assets/icons/popup-design.png"
-            width={263}
-            height={626}
-            alt="Icon"
-            className="absolute -left-10 top-5 h-full w-3/12 sm:left-0"
-          />
-          <Image
-            src="/assets/icons/popup-design.png"
-            width={263}
-            height={626}
-            alt="Icon"
-            className="absolute -right-10 top-5 aspect-auto h-full w-3/12 scale-x-[-1] sm:right-0"
-          />
-        </div>
-      </PopupTwo>
-      <PopupTwo
+      />
+      <InReview
         isOpen={
           showErrorPopup && fetchedUserData?.verificationStatus === "PENDING"
         }
         onClose={() => setShowErrorPopup(false)}
-      >
-        <div className="relative max-h-[700px] min-w-[320px] max-w-[700px] bg-white p-5 sm:min-w-[560px]">
-          <h3 className="mb-7 mt-4 text-center font-clashSemiBold text-2xl text-[#2A1769] sm:text-4xl">
-            Verification in Review.
-          </h3>
-          <p className="mx-auto mb-10 max-w-[383px] text-center font-satoshiMedium text-base text-[#140B31] sm:text-lg md:text-xl">
-            We’re verifying your ID and would get back to you via email within
-            24 hrs. Until it’s complete, actions like making an offer are
-            unavailable. Thanks for your patience!
-          </p>
-          <div className="flex justify-center gap-5">
-            <button
-              onClick={() => setShowErrorPopup(false)}
-              className="rounded-full border-[0.5px] border-primary bg-[#EBE9F4] px-5 py-2 font-bold text-primary"
-            >
-              Back
-            </button>
-            <Link
-              href="/service-provider/profile"
-              className="rounded-full bg-[#381F8C] px-5 py-2 font-bold text-[#EBE9F4]"
-            >
-              Go To Profile
-            </Link>
-          </div>
-          <Image
-            src="/assets/icons/popup-design.png"
-            width={263}
-            height={626}
-            alt="Icon"
-            className="absolute -left-10 top-5 h-full w-3/12 sm:left-0"
-          />
-          <Image
-            src="/assets/icons/popup-design.png"
-            width={263}
-            height={626}
-            alt="Icon"
-            className="absolute -right-10 top-5 aspect-auto h-full w-3/12 scale-x-[-1] sm:right-0"
-          />
-        </div>
-      </PopupTwo>
+      />
     </section>
   );
 };
