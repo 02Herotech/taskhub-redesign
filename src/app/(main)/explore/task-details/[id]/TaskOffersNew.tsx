ac3e10026048dd1bca4a99ba4f02a0a626a3bba0
damzymichael <105460672+damzymichael@users.noreply.github.com>
2025-03-03 08:45:33 +0100

    ....

--- src/app/(main)/explore/task-details/[id]/TaskOffersNew.tsx
+++ src/app/(main)/explore/task-details/[id]/TaskOffersNew.tsx
@@ -34,7 +34,7 @@ function TaskOffersNew({ currentUserId, taskId, posterId }: OffersProps) {
   }, []);
   return (
     <section className="mt-14">
-      <header className="mb-6 text-[#E58C06]">
+      <header className="mb-8 text-[#E58C06]">
         <div className="mb-3 flex items-center justify-between">
           <h2 className="font-satoshiBold text-xl font-bold lg:text-3xl">
             Offers
