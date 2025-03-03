0fcd94a6c5a5f892bb35bc2c3c76eb2106c645fa
Gideon Moses <gideon.moses@miatech.pro>
2025-03-03 10:32:42 +0100

    rewards in dashboard done

---
+++ src/app/(dashboard)/service-provider/rewards/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import RewardsComponent from "@/components/dashboard/general/Rewards";
+import React from "react";
+
+const ServiceProviderRewards = () => {
+  return <RewardsComponent />;
+};
+
+export default ServiceProviderRewards;
