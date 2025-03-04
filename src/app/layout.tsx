ec51fafaf0af5142c5247f3cb2051f65cbe44987
Akinyemi samuel Ayodele <97408729+Akinyemi-samuel@users.noreply.github.com>
2025-03-04 13:09:17 +0100

    added google pixel to the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,44 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+            <head>
+          <meta name="facebook-domain-verification" content="fvdmvueltfhf25xud8zdn88ms54tib" />
+              
+          <script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;
+  n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  
+  fbq('init', '532276502710889'); 
+  fbq('track', 'PageView');
+</script>
+<noscript>
+  <img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=532276502710889&ev=PageView&noscript=1"/>
+</noscript>
+
+          {/* Google Tag Manager */}
+          <script
+              async
+              src='https://www.googletagmanager.com/gtag/js?id=G-9DNH0S7F2B'
+          ></script>
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-9DNH0S7F2B');
+    `
+              }}
+          />
+      </head>
       <body>
         <Provider>
           <TokenExpirationCheck>
