e6ab72cd6cd2e3ed36b4902d6f64590d9a8e69d2
damzymichael <105460672+damzymichael@users.noreply.github.com>
2025-03-19 22:58:41 +0100

    Router flow test

--- src/app/auth/sign-up/SignUp.tsx
+++ src/app/auth/sign-up/SignUp.tsx
@@ -112,8 +112,8 @@ function SignUp() {
     try {
       await signup(finalData).unwrap();
       setError("");
-      reset();
-      router.push(`/auth/verify-email?email=${finalData.emailAddress}`);
+      // reset();
+      router.replace(`/auth/verify-email?email=${finalData.emailAddress}`);
     } catch (error: any) {
       console.error("Error while signing up: ", error);
       setError(error?.data?.message || "An unexpected error occurred");
