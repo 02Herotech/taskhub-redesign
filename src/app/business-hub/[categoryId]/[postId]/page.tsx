f5c4ee06b94ea387c68f073b72cbc1747f40909f
damzymichael <105460672+damzymichael@users.noreply.github.com>
2025-04-29 18:05:48 +0100

    Business hub Implmentation

---
+++ src/app/business-hub/[categoryId]/[postId]/page.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import Image from "next/image";
+import RichTextRenderer from "@/components/blog/RichText";
+
+type Props = {
+  params: { categoryId: "business"; postId: "business--finance" };
+};
+
+type Image = {
+  id: string;
+  alt: string;
+  filename: string;
+  mimeType: string;
+  filesize: number;
+  width: number;
+  height: number;
+  focalX: number;
+  focalY: number;
+  createdAt: string;
+  updatedAt: string;
+  url: string;
+};
+
+type Category = {
+  id: string;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+  image: Image;
+  longTitle: string;
+  shortDescription: string;
+  slug: string;
+  blogsHeader: string;
+};
+
+type Author = {
+  id: string;
+  name: string;
+  image: Record<string, unknown>;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+  loginAttempts: number;
+};
+
+type Meta = {
+  image: Image;
+};
+
+type Post = {
+  id: string;
+  title: string;
+  postSummary: string;
+  postContent: any[];
+  image: Image;
+  featuredPost: boolean;
+  readTime: string;
+  category: Category;
+  publishedAt: string;
+  authors: Author[];
+  slug: string;
+  meta: Meta;
+  _status: string;
+  createdAt: string;
+  updatedAt: string;
+  populatedAuthors: any[];
+};
+
+async function Page({ params }: Props) {
+  const result = await fetch(
+    `${process.env.BLOG_API}/posts?where[slug][equals]=${params.postId}`,
+  );
+  const posts: { docs: Post[] } = await result.json();
+
+  const post = posts.docs[0];
+  return (
+    <main className="mx-auto max-w-7xl px-5 md:px-20">
+      <article>
+        <header className="my-10">
+          <h1 className="mx-auto mb-8 max-w-3xl text-center font-clashSemiBold text-2xl md:text-4xl">
+            {post.title}
+          </h1>
+          <Image
+            src={post.image.url}
+            width={post.image.width}
+            height={post.image.height}
+            alt="Business page header image"
+            className="mx-auto h-[250px] w-full rounded-3xl object-cover object-center md:h-[400px]"
+          />
+        </header>
+        <div className="mx-auto max-w-screen-sm pb-10 text-base font-medium md:text-lg">
+          <RichTextRenderer content={post.postContent} />
+        </div>
+      </article>
+    </main>
+  );
+}
+
+export default Page;
