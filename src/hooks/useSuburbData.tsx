7b71ccc3a4273b929b543fdbcb9a738f803aa7cc
damzymichael <105460672+damzymichael@users.noreply.github.com>
2025-05-16 11:03:36 +0100

    Fix abn, fix address

--- src/hooks/useSuburbData.tsx
+++ src/hooks/useSuburbData.tsx
@@ -31,7 +31,7 @@ function useSuburbData(searchValue: string, currentSuburb: SurburbInfo | null) {
         if (suburbList.length < 1) setIsLoading(true);
         const { data } = await axios.get<SurburbInfo[]>(
           process.env.NEXT_PUBLIC_API_URL +
-            "/util/addresses?query=" +
+            "/util/addresses?suburb=" +
             searchValue,
         );
         setIsLoading(false);
